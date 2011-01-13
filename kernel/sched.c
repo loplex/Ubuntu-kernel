0759b12670c11e9139a914ca2e60ada67aea2fd4
Stefan Bader <stefan.bader@canonical.com>
2011-01-13 16:59:06 -0600

    UBUNTU: SAUCE: sched: Try tp catch cpu_power being set to 0
    This is an optional change to try catching the culprit which changes
    cpu_power to 0 (should never happen) and causes divide by zero crashes
    later on in the scheduler code.
    
    BugLink: http://bugs.launchpad.net/bugs/614853
    
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Andy Whitcroft <andy.whitcroft@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- kernel/sched.c
+++ kernel/sched.c
@@ -3798,6 +3798,7 @@ static void update_cpu_power(struct sched_domain *sd, int cpu)
 	unsigned long weight = sd->span_weight;
 	unsigned long power = SCHED_LOAD_SCALE;
 	struct sched_group *sdg = sd->groups;
+	unsigned long scale_rt;
 
 	if (sched_feat(ARCH_POWER))
 		power *= arch_scale_freq_power(sd, cpu);
@@ -3815,11 +3816,17 @@ static void update_cpu_power(struct sched_domain *sd, int cpu)
 		power >>= SCHED_LOAD_SHIFT;
 	}
 
-	power *= scale_rt_power(cpu);
+	scale_rt = scale_rt_power(cpu);
+	power *= scale_rt;
+
 	power >>= SCHED_LOAD_SHIFT;
 
 	if (!power)
 		power = 1;
+
+	if (WARN_ON((long) power <= 0))
+		printk(KERN_ERR "cpu_power = %ld; scale_rt = %ld\n",
+			power, scale_rt);
 
 	cpu_rq(cpu)->cpu_power = power;
 	sdg->cpu_power = power;
@@ -3845,6 +3852,9 @@ static void update_group_power(struct sched_domain *sd, int cpu)
 	} while (group != child->groups);
 
 	sdg->cpu_power = power;
+
+	if (WARN_ON((long) power <= 0))
+		printk(KERN_ERR "cpu_power = %ld\n", power);
 }
 
 /**
