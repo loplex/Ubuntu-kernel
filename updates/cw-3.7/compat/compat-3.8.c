2cf833e6e830b2570d1d4c777aef2db3cc646fa6
Leann Ogasawara <leann.ogasawara@canonical.com>
2013-04-05 09:58:18 -0700

    UBUNTU: Add compat-drivers v3.7
    BugLink: http://bugs.launchpad.net/bugs/1165084
    
    Add the initial compat-drivers v3.7 package.  This is based on the
    following:
    
      linux-stable: git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git v3.7.9
      compat: https://github.com/mcgrof/compat.git compat-v3.7.9-1
      compat-drivers: git://github.com/mcgrof/compat-drivers.git compat-drivers-v3.7.9-1
    
    And was manually created following the steps documented at:
    
      http://linuxwireless.org/en/users/Download/hacking#Generating_stable_releases
    
        ie. cd devel/compat-drivers && ./scripts/gen-stable-release.sh
    
    Manual creation was done because the compat-drivers archive wasn't
    current, ie. http://www.orbit-lab.org/kernel/compat-drivers/
    
    Signed-off-by: Leann Ogasawara <leann.ogasawara@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

---
+++ updates/cw-3.7/compat/compat-3.8.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2018  Luis R. Rodriguez <mcgrof@do-not-panic.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Compatibility file for Linux wireless for kernels 3.8.
+ */
+
+#include <linux/netdevice.h>
+#include <linux/export.h>
+
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,7,8))
+void netdev_set_default_ethtool_ops(struct net_device *dev,
+				    const struct ethtool_ops *ops)
+{
+	if (!dev->ethtool_ops)
+		dev->ethtool_ops = ops;
+}
+EXPORT_SYMBOL_GPL(netdev_set_default_ethtool_ops);
+#endif
